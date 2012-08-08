9853d5b756b4b68a09ef332863691d6fe7b4e110
sybil63 <xichen0603@gmail.com>
2012-08-08 18:28:22 +0800

    MOD: get rewrite url

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-    echo phpinfo();
-?>
+function _url()
+{
+    if (!empty($_SERVER['PATH_INFO'])) {
+        return $_SERVER['PATH_INFO'];
+    }else if (!empty($_SERVER['REQUEST_URI'])) {
+        return $_SERVER['REQUEST_URI'];
+    }elseif (isset($_SERVER['PHP_SELF']) && isset($_SERVER['SCRIPT_NAME'])) {
+            $uri = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']);
+    } elseif (isset($_SERVER['HTTP_X_REWRITE_URL'])) {
+            $uri = $_SERVER['HTTP_X_REWRITE_URL'];
+    } elseif ($var = env('argv')) {
+            $uri = $var[0];
+    }
+    return $uri;
+}
+$url = _url();
+echo $url;
